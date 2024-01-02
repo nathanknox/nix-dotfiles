2a7a44b8a1d04c4c712ed74f2d4d417051e4f31d
Nathan Knox <nathan.knox@gmail.com>
2024-01-02 15:57:44 -0500

    nope

--- common.nix
+++ common.nix
@@ -1,10 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "nknox";
-  home.homeDirectory = "/home/nknox";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
