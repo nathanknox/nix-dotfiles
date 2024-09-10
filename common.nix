941359eac8472336745c74ffb31437a10eb649a2
Nathan Knox <nathan.knox@gmail.com>
2024-09-10 08:04:33 -0400

    add azure-cli

--- common.nix
+++ common.nix
@@ -22,6 +22,7 @@
     pkgs.ripgrep
     pkgs.glow
     pkgs.httpie
+    pkgs.azure-cli
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
