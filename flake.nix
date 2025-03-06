b06824c128fd5ea8924243e90f2894e68bb1cb86
Nathan Knox <nathan.knox@gmail.com>
2025-03-06 11:26:47 -0500

    fix!: aligned Home Manager version to NixPkgs version

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
